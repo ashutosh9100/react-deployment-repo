ada31949837b72b1034979b7da87aca8c1ca68cc
ashutosh9100 <ap059410@gmail.com>
2020-05-15 06:04:26 +0530

    commit

--- server.js
+++ server.js
@@ -26,9 +26,10 @@ mongoose
 
   if(process.env.NODE_ENV==='production'){
       app.use(express.static('client/build'))
-      app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-      })
+      app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
+          res.sendFile(path.join(__dirname, './client/build/index.html'));
+        });
+  console.log('Serving React App...');
   }
 
 app.listen(port);
