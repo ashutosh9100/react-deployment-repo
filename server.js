bf1e3fdcb2ae80b6aa573daf45d3aaaf7e87a048
ashutosh9100 <ap059410@gmail.com>
2020-05-15 07:55:34 +0530

    kchange build folder to public

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ mongoose
  
   if (process.env.NODE_ENV === "production") {
     // Set static folder
-    app.use('/', express.static("./client/build"));
+    app.use('/', express.static("./public"));
   
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client","build", "index.html"));
+      res.sendFile(path.resolve(__dirname, "public", "index.html"));
     }); 
   }
   
